9ce07071773711d4161477acfbed60791eec5820
luz55 <lucasluz55@gmail.com>
2023-05-14 15:00:02 -0300

    add rust tools

--- arch_base.sh
+++ arch_base.sh
@@ -1,11 +1,11 @@
 # update package manager
 sudo pacman -Syyu --noconfirm
-#
+
 mkdir -v ~/.config/
 mkdir -pv ~/.local/share/
 mkdir -pv ~/.local/share/fonts/
-#
-## install git
+
+# install git
 sudo pacman -S git --noconfirm --needed
 
 cd ~/
@@ -19,6 +19,8 @@ sudo pacman -S --noconfirm --needed exa
 sudo pacman -S --noconfirm --needed fish
 sudo pacman -S --noconfirm --needed kitty
 sudo pacman -S --noconfirm --needed telegram-desktop
+# (de)compression tool
+# sudo pacman -S --noconfirm --needed ouch
 
 # For use with i3-wm
 # sudo pacman -S --noconfirm --needed i3-wm picom autotiling feh
@@ -27,18 +29,20 @@ sudo pacman -S --noconfirm --needed fd
 sudo pacman -S --noconfirm --needed ripgrep
 sudo pacman -S --noconfirm --needed firefox
 sudo pacman -S --noconfirm --needed base-devel
-
 # customizable lockscreen
 # sudo pacman -S --noconfirm --needed gtklock
-#
 # screenshot
 sudo pacman -S --noconfirm --needed flameshot
+
 sudo pacman -S --noconfirm --needed xrandr
 sudo pacman -S --noconfirm --needed neofetch
 
-# download packages from AUR
+## Download packages from AUR
 sudo yay -S --noconfirm --needed ungoogled-chromium-bin
-sudo yay -S --noconfirm --needed ttf-jetbrains-mono-nerd
+sudo yay -S --noconfirm --needed ttf-firacode-nerd
+# powerfull for directory navigation
+sudo yay -S --noconfirm --needed broot
+sudo yay -S --noconfirm --needed topgrade
 
 # set fish as default shell
 chsh -s /usr/bin/fish
